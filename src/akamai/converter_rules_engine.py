eaedea20e3f08803fe7ac9dc4b4efd8ac24f3981
Test User <test@example.com>
2025-06-24 11:57:13 -0300

    fix: W0621

--- src/akamai/converter_rules_engine.py
+++ src/akamai/converter_rules_engine.py
@@ -45,7 +45,7 @@ def create_order_factory():
         return value
     
     return create_order
-create_order = create_order_factory()
+create_rule_order = create_order_factory()
 
 
 def create_rule_engine(
@@ -213,7 +213,7 @@ def assemble_request_rule(
             phase = "default"
             order = 0
         else:
-            order = create_order()
+            order = create_rule_order()
 
         resource = {
             "type": "azion_edge_application_rule_engine",
@@ -241,7 +241,7 @@ def assemble_request_rule(
         
         # Create a rule for each special behavior, combined with all standard behaviors
         for idx, special_behavior in enumerate(special_behaviors):
-            order = create_order()
+            order = create_rule_order()
             suffix = f"_{order}_sp{idx+1}"
             unique_rule_name = rule_name + suffix
 
@@ -329,7 +329,7 @@ def assemble_response_rule(
                 "description": rule_description,
                 "phase": "response",
                 "behaviors": behaviors,
-                "order": create_order()
+                "order": create_rule_order()
             },
             "depends_on": depends_on,
             "order": index
